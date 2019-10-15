ed719bd150a33001b69bc544c040513789674679
guzov435 <guzov435@gmail.com>
2019-10-15 20:59:18 +0300

    full complete task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let output_string = ''; 
+    let sub_string = '';
+
+    for (let i = 0; i < expr.length ; i += 2 )
+    {
+            if (expr[i] == 0 && expr[i+1] == 0){
+            continue;
+            }
+            else{
+                if (expr[i] == 1 && expr[i+1] == 0){
+                    sub_string += '.';
+                }
+                else{
+                    if (expr[i] == 1 && expr[i+1] == 1){
+                        sub_string += '-';
+                    }
+                    else {
+                        if (expr[i] == '*'){
+                            i += 10;
+                            output_string += ' ';
+                        }
+                    }
+                }
+            }
+
+            if(i != 0 && i % 10 == 8 || i == expr.length - 2){
+                output_string += MORSE_TABLE[sub_string];
+                sub_string = '';
+            }
+    }
+
+    return output_string;
 }
 
 module.exports = {
     decode
 }
+
+
+/*var output_string = ''
+    var new_array = string.split('**********')
+    for(var i = 0; i < new_array.length; i++){
+        output_string += MORSE_TABLE[new_array[i]]
+    }
+    return output_string*/
